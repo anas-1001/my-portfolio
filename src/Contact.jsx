93e119f8e64e56f043ab747acfc9b4bd5edff62e
anas-1001 <anasansari28642@gmail.com>
2025-11-18 21:11:08 +0530

    updated instagram

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
         <div className="contact">
             <h2>Contact Me</h2>
             <p>Email:-<a href="mailto:anasansari28642@gmail.com">anasansari28642@gmail.com</a></p>
-            <p>Instagram:-<a href="https://www.instagram.com/_the_anas_ansari_/" target="_blank" rel="noopener noreferrer">linkedin.com/in/anasansari</a></p>
+            <p>Instagram:-<a href="https://www.instagram.com/_the_anas_ansari_/" target="_blank" rel="noopener noreferrer">_the_anas_ansari_</a></p>
             <p>Phone No:-<a href="tel:9720218370">9720218370</a></p>
 
         </div>
